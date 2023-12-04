cab059b33fdea484d5547bbb5169593aabae6357
kreuzerk <kevin.kreuzer90@icloud.com>
2023-12-04 16:34:55 +0100

    feat: 🎸 setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'foo',
+    loadComponent: () => import('./features/foo/foo.component').then(c => c.FooComponent)
+  },
+  {
+    path: '**',
+    redirectTo: 'foo',
+    pathMatch: 'full'
+  }
+];
